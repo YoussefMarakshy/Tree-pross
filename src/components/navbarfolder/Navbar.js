648e5ec442abcd6a25ca41b94b8423467ecfedeb
Mostafa El-Messiry <mostafamesiry@gmail.com>
2018-07-03 21:51:32 +0200

    fixed cursor on hover

--- src/components/navbarfolder/Navbar.js
+++ src/components/navbarfolder/Navbar.js
@@ -10,10 +10,10 @@ const NavBar = () => {
        			<div className="nav-bar">
 				<img src="./images/logo.png" alt="" className="nav-bar__logo"/>
 				<nav className="nav-bar__buttons cl-effect-7">
-				<a><Link className="" to="tree-removal">Tree Removal</Link></a>
-				<a><Link className="" to="stump-grinding">Stump Grinding</Link></a>
-				<a><Link className="" to="tree-trimming">Tree Trimming</Link></a>
-				<a><Link className="" to="storm-damage">Storm Damage</Link></a>
+				<a><Link className="nav-list" to="tree-removal">Tree Removal</Link></a>
+				<a><Link className="nav-list" to="stump-grinding">Stump Grinding</Link></a>
+				<a><Link className="nav-list" to="tree-trimming">Tree Trimming</Link></a>
+				<a><Link className="nav-list" to="storm-damage">Storm Damage</Link></a>
 				</nav>
 				</div>
      		 </section>
