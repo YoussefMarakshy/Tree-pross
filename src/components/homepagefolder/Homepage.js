b8c27adcb6c6d6b071695b438957d268f8a1f66b
Mostafa El-Messiry <mostafamesiry@gmail.com>
2018-07-07 13:18:12 +0200

    slider component now takes slides as array of elements

--- src/components/homepagefolder/Homepage.js
+++ src/components/homepagefolder/Homepage.js
@@ -3,44 +3,19 @@ import './Homepage.css';
 import Slider from './Slider';
 
 
-const content = [
-	{
-		title: 'Vulputate Mollis Ultricies Fermentum Parturient',
-		description:
-		'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.',
-		button: 'Read More',
-		image: 'http://img1.wsimg.com/isteam/ip/dc7b5cfe-333c-41f1-a668-08723e9532f2/7bb98dce-421b-4dfa-a32d-08ca5a33e163.jpg',
-		user: 'Luan Gjokaj',
-		userProfile: 'https://i.imgur.com/JSW6mEk.png'
-	},
-	{
-		title: 'Tortor Dapibus Commodo Aenean Quam',
-		description:
-		'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.',
-		button: 'Discover',
-		image: 'https://sunstatecompanies.com/wp-content/uploads/2013/03/Tree-Trimming.jpg',
-		user: 'Erich Behrens',
-		userProfile: 'https://i.imgur.com/0Clfnu7.png'
-	},
-	{
-		title: 'Phasellus volutpat metus',
-		description:
-		'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.',
-		button: 'Buy now',
-		image: 'https://images.unsplash.com/photo-1521633603986-cb82a097ffba?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c80f5195342687349b3b059f358aea4b&auto=format&fit=crop&w=1350&q=80',
-		user: 'Bruno Vizovskyy',
-		userProfile: 'https://i.imgur.com/4KeKvtH.png'
-	}
+const slides = [
+	<div className="slide-content">Hello</div>,
+	<div className="slide-content">hi</div>,
+	<div className="slide-content">sup</div>,
+	<div className="slide-content">true</div>
 ];
 
 
 class Homepage extends Component{
 	render(){
 		return (
-	<div className="homepage-section">
-		<Slider />
-	</div>
-	);
+			<Slider slides={slides}/>
+		);
 		
 	}
 }
