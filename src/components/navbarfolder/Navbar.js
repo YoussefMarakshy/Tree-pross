6d464c1f624ed1fc561e42f6d9765ebe29e912c0
Mostafa El-Messiry <mostafamesiry@gmail.com>
2018-09-21 22:01:16 +0200

    navbar is now responsive

--- src/components/navbarfolder/Navbar.js
+++ src/components/navbarfolder/Navbar.js
@@ -7,17 +7,42 @@ const NavBar = () => {
 	return (
 		<header>
      	 	<section>
-       			<div className="nav-bar">
-					<Link className="" to ="homepage"> <img src="./images/logo.png" alt="" className="nav-bar__logo"/> </Link>
+       			{/*<div className="nav-bar">
+					
 					<nav className="nav-bar__buttons cl-effect-7">
-						<a><Link className="nav-list" to="tree-removal">Tree Removal</Link></a>
-						<a><Link className="nav-list" to="stump-grinding">Stump Grinding</Link></a>
+						
+						
 						<a><Link className="nav-list" to="tree-trimming">Trimming & Pruning</Link></a>
 						<a><Link className="nav-list" to="storm-damage">Storm Damage</Link></a>
 						<a><Link className="nav-list" to="footer"> Contact Us <br/> 608-466-0301 </Link></a>
 					</nav>
 					<i className="fas fa-bars menu-button"></i>
-				</div>
+				</div>*/}
+				<nav className="navbar fixed-top navbar-expand-lg navbar-dark justify-content-between nav-bar">
+				  <a className="navbar-brand" href="#"><Link className="" to ="homepage"> <img src="./images/logo.png" alt="" className="nav-bar__logo"/> </Link></a>
+				  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+				    <span className="navbar-toggler-icon"></span>
+				  </button>
+				  <div className="collapse navbar-collapse navbar-right" id="navbarNavDropdown">
+				    <ul className="nav navbar-nav">
+				      <li className="nav-item align-middle">
+				        <a className="nav-link"><Link className="nav-list nav-bar-item" to="tree-removal">Tree Removal</Link></a>
+				      </li>
+				      <li className="nav-item align-middle">
+				        <a className="nav-link"><Link className="nav-list nav-bar-item" to="stump-grinding">Stump Grinding</Link></a>
+				      </li>
+				      <li className="nav-item align-middle">
+				        <a className="nav-link"><Link className="nav-list nav-bar-item" to="tree-trimming">Trimming & Pruning</Link></a>
+				      </li>
+				      <li className="nav-item align-middle">
+				        <a className="nav-link"><Link className="nav-list nav-bar-item" to="storm-damage">Storm Damage</Link></a>
+				      </li>
+  				      <li className="nav-item">
+				        <a className="nav-link"><Link className="nav-list nav-bar-item" to="footer"><div> Contact Us <br/> 608-466-0301 </div></Link></a>
+				      </li>
+				    </ul>
+				  </div>
+				</nav>
      		 </section>
     	</header>
 	)
